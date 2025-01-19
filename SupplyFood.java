7112624f5d6a8de340b316530a01ab92f5175a26
Satyam Panchal <satyam080424@gmail.com>
2025-01-19 22:00:22 +0530

    add supply class

---
+++ SupplyFood.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Supply
+{
+    int Code;
+    String FoodName;
+    String Sticker;
+    String FoodType;
+    
+    public void GetType(){
+        if(Sticker.equals("Green"))
+           FoodType = "Vegetarian";
+        if(Sticker.equals("Yellow"))
+           FoodType = "Contains Egg";
+        if(Sticker.equals("Red"))
+           FoodType = "Non Vegetarian";
+    }
+
+    public void FoodIn(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Food Code : ");
+        Code=sc.nextInt();
+        System.out.println("Enter Food Name : ");
+        FoodName=sc.next();
+        System.out.println("Enter Sticker Color : ");
+        Sticker=sc.next();
+        GetType();
+    }
+    
+
+    public void FoodOut(){
+        System.out.println("You Entered ");
+        System.out.println("Food Code     : "+Code);
+        System.out.println("Food Name     : "+FoodName);
+        System.out.println("Sticker Color : "+Sticker);
+        System.out.println("Food Type     : "+FoodType);
+    }
+}
+
+class SupplyFood
+{
+   public static void main(String args[])
+   {
+     Supply ob=new Supply();  
+     ob.FoodIn();
+     ob.FoodOut();
+   }
+}
