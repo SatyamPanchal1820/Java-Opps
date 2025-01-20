7a58d06cf6970713760b5337a2d7356fa0bcfd27
Satyam Panchal <satyam080424@gmail.com>
2025-01-20 21:53:46 +0530

    update a file

--- SupplyFood.java
+++ SupplyFood.java
@@ -1,49 +1,46 @@
 import java.util.*;
 
-class Supply
-{
+class Supply {
     int Code;
     String FoodName;
     String Sticker;
     String FoodType;
-    
-    public String GetType(){
-        if(Sticker.equals("Green"))
-           FoodType = "Vegetarian";
-        if(Sticker.equals("Yellow"))
-           FoodType = "Contains Egg";
-        if(Sticker.equals("Red"))
-           FoodType = "Non Vegetarian";
 
-           return FoodType;
+    public String GetType() {
+        if (Sticker.equals("Green"))
+            FoodType = "Vegetarian";
+        if (Sticker.equals("Yellow"))
+            FoodType = "Contains Egg";
+        if (Sticker.equals("Red"))
+            FoodType = "Non Vegetarian";
+
+        return FoodType;
     }
 
-    public void FoodIn(){
+    public void FoodIn() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Food Code : ");
-        Code=sc.nextInt();
+        Code = sc.nextInt();
         System.out.println("Enter Food Name : ");
-        FoodName=sc.next();
+        FoodName = sc.next();
         System.out.println("Enter Sticker Color : ");
-        Sticker=sc.next();
+        Sticker = sc.next();
         GetType();
     }
-    
-    public void FoodOut(){
+
+    public void FoodOut() {
         System.out.println("You Entered ");
-        System.out.println("Food Code     : "+Code);
-        System.out.println("Food Name     : "+FoodName);
-        System.out.println("Sticker Color : "+Sticker);
-        System.out.println("Food Type     : "+FoodType);
+        System.out.println("Food Code     : " + Code);
+        System.out.println("Food Name     : " + FoodName);
+        System.out.println("Sticker Color : " + Sticker);
+        System.out.println("Food Type     : " + FoodType);
     }
 }
 
-class SupplyFood
-{
-       public static void main(String args[])
-   {
-     Supply ob=new Supply();  
-     ob.FoodIn();
-     ob.FoodOut();
-   }
+class SupplyFood {
+    public static void main(String args[]) {
+        Supply ob = new Supply();
+        ob.FoodIn();
+        ob.FoodOut();
+    }
 }
