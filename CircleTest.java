d52799c4ccc5f814c1d30065f9ffb99906275c46
Satyam Panchal <satyam080424@gmail.com>
2025-01-20 11:51:11 +0530

    address comment on PR

---
+++ CircleTest.java
@@ -0,0 +1,41 @@
+
+class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void displayDetails() {
+        System.out.println("Radius : " + radius);
+      
+    }
+}
+
+public class CircleTest {
+    public static void main(String[] args) {
+       Circle circle = new Circle(10.0);
+            circle.displayDetails();
+            System.out.println("Area of circle is " + circle.getArea() + " sq. units");
+            Circle circle2 = new Circle(20.0);
+            circle2.displayDetails();
+            System.out.println("Area of circle is " + circle.getArea() + " sq. units");
+    }
+}
