aab51bc2f08556dcb31e1f0879a7815489696e8d
Satyam Panchal <satyam080424@gmail.com>
2025-01-22 11:22:18 +0530

    another practice question

--- SalaryRaise.java
+++ SalaryRaise.java
@@ -1,53 +1,62 @@
-class Salary{
+class Salary {
     private String name;
     private String job;
     private double salary;
-    public Salary(String name,String job,double salary){
-        this.name=name;
-        this.job=job;
-        this.salary=salary;
+
+    public Salary(String name, String job, double salary) {
+        this.name = name;
+        this.job = job;
+        this.salary = salary;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-    public String getJob(){
+
+    public String getJob() {
         return job;
     }
-    public double getSalary(){
+
+    public double getSalary() {
         return salary;
     }
 
-    public void setName(String name){
-        this.name=name;
+    public void setName(String name) {
+        this.name = name;
     }
-    public void setJob(String job){
-        this.job=job;
+
+    public void setJob(String job) {
+        this.job = job;
     }
-    public void setSalary(double salary){
-        this.salary=salary;
+
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
-    public void raiseSalary(double per){
-        salary=salary+salary+(per/100);
+
+    public void raiseSalary(double per) {
+        salary = salary + salary + (per / 100);
     }
 }
 
 public class SalaryRaise {
     public static void main(String[] args) {
-        Salary salary=new Salary("Satyam Panchal", "IT",60000);
-        System.out.println("Name of Employee is "+salary.getName()+", job title is "+salary.getJob()+" and salary is "+salary.getSalary());
+        Salary salary = new Salary("Satyam Panchal", "IT", 60000);
+        System.out.println("Name of Employee is " + salary.getName() + ", job title is " + salary.getJob()
+                + " and salary is " + salary.getSalary());
 
         salary.raiseSalary(15);
-           System.out.println("Name of Employee is "+salary.getName()+", job title is "+salary.getJob()+" and salary is "+salary.getSalary());
+        System.out.println("Name of Employee is " + salary.getName() + ", job title is " + salary.getJob()
+                + " and salary is " + salary.getSalary());
 
-           
         salary.setName("John Doe");
         salary.setJob("Marketing");
         salary.setSalary(40000);
-        
-        System.out.println("Name of Employee is "+salary.getName()+", job title is "+salary.getJob()+" and salary is "+salary.getSalary());
+
+        System.out.println("Name of Employee is " + salary.getName() + ", job title is " + salary.getJob()
+                + " and salary is " + salary.getSalary());
         salary.raiseSalary(8);
-        System.out.println("Name of Employee is "+salary.getName()+", job title is "+salary.getJob()+" and salary is "+salary.getSalary());
-        
+        System.out.println("Name of Employee is " + salary.getName() + ", job title is " + salary.getJob()
+                + " and salary is " + salary.getSalary());
+
     }
 }
