2eabdd650b0c5f96c25689933cd7e06d1b4020c7
Satyam Panchal <satyam080424@gmail.com>
2025-01-23 14:07:10 +0530

    Practice question

--- TrafficLight.java
+++ TrafficLight.java
@@ -1,61 +1,64 @@
- class Light {
-    private String colour;
-    private int duration;
-    
-    public Light(String colour,int duration){
-        this.colour=colour;
-        this.duration=duration;
+class Light {
+  private String colour;
+  private int duration;
 
-    }
-    public String getColour(){
-        return colour;
-    }
-    public int getDuration(){
-        return duration;
-    }
-    void setColour(String colour){
-        this.colour=colour;
-        
-    }
-    void  setDuration(int duration){
-        this.duration=duration;
-    }
-    public void changeColor(String newColour) {
-      
-      colour = newColour;
-    }
-    
+  public Light(String colour, int duration) {
+    this.colour = colour;
+    this.duration = duration;
+
+  }
+
+  public String getColour() {
+    return colour;
+  }
+
+  public int getDuration() {
+    return duration;
+  }
+
+  void setColour(String colour) {
+    this.colour = colour;
+
+  }
+
+  void setDuration(int duration) {
+    this.duration = duration;
+  }
+
+  public void changeColor(String newColour) {
+
+    colour = newColour;
+  }
+
   public boolean isRed() {
 
     return colour.equals("red");
   }
 
   public boolean isGreen() {
- 
+
     return colour.equals("green");
   }
-  
+
 }
 
-public class  TrafficLight{
-  
+public class TrafficLight {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-     Light light = new Light("red", 30);
+    Light light = new Light("red", 30);
 
-      System.out.println("The light is red: " + light.isRed());
+    System.out.println("The light is red: " + light.isRed());
 
-      System.out.println("The light is green: " + light.isGreen());
+    System.out.println("The light is green: " + light.isGreen());
 
-      light.changeColor("green");
+    light.changeColor("green");
 
-      System.out.println("The light is now green: " + light.isGreen());
+    System.out.println("The light is now green: " + light.isGreen());
 
-      System.out.println("The light duration is: " + light.getDuration());
+    System.out.println("The light duration is: " + light.getDuration());
 
-      light.setDuration(20);
-      System.out.println("The light duration is now: " + light.getDuration());
-    }
+    light.setDuration(20);
+    System.out.println("The light duration is now: " + light.getDuration());
   }
-  
+}
